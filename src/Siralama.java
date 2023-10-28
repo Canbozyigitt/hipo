ae09064a1329f5c604b7722026fc4b2bf900bc93
CANO <umutcanbozyigit34012@gmail.com>
2023-10-28 12:38:31 +0300

    siralama

---
+++ src/Siralama.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Siralama {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Birinci sayıyı girin: ");
+        int sayi1 = scanner.nextInt();
+
+        System.out.print("İkinci sayıyı girin: ");
+        int sayi2 = scanner.nextInt();
+
+        System.out.print("Üçüncü sayıyı girin: ");
+        int sayi3 = scanner.nextInt();
+
+        // Küçükten büyüğe sıralama
+        if (sayi1 > sayi2) {
+            int gecici = sayi1;
+            sayi1 = sayi2;
+            sayi2 = gecici;
+        }
+        if (sayi1 > sayi3) {
+            int gecici = sayi1;
+            sayi1 = sayi3;
+            sayi3 = gecici;
+        }
+        if (sayi2 > sayi3) {
+            int gecici = sayi2;
+            sayi2 = sayi3;
+            sayi3 = gecici;
+        }
+
+        System.out.println("Küçükten büyüğe sıralama: " + sayi1 + ", " + sayi2 + ", " + sayi3);
+
+        scanner.close();
+    }
+}

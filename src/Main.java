389351fbdcd82c0eb529785e44b7eaba4dd61cc3
CANO <umutcanbozyigit34012@gmail.com>
2023-10-28 12:12:42 +0300

    hipotenüs

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("İlk dik kenarın uzunluğunu girin: ");
+        double kenar1 = scanner.nextDouble();
+
+        System.out.print("İkinci dik kenarın uzunluğunu girin: ");
+        double kenar2 = scanner.nextDouble();
+
+        // Hipotenüs hesaplama formülü: c^2 = a^2 + b^2
+        double hipotenüs = Math.sqrt(Math.pow(kenar1, 2) + Math.pow(kenar2, 2));
+
+        System.out.println("Hipotenüs uzunluğu: " + hipotenüs);
+
+        scanner.close();
+    }
+}

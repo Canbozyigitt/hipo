c6d1d12c017207dad13b589193720827c4700e01
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-01-04 11:56:37 +0300

    Update Siralama.java

--- src/Siralama.java
+++ src/Siralama.java
@@ -4,7 +4,7 @@ public class Siralama {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Birinci sayıyı girin: ");
+        System.out.print("Birinci sayıyı giriniz: ");
         int sayi1 = scanner.nextInt();
 
         System.out.print("İkinci sayıyı giriniz: ");
